9ee6531861ca3a849c97606700d1d01e4c296ce4
sar <sar@starfinanz.de>
2022-07-29 17:13:35 +0200

    unsafe test impl

--- app/src/main/java/com/sar/steamaccountswitcher/steam/data/remote/service/SteamAccountSwitcherAPIFactory.kt
+++ app/src/main/java/com/sar/steamaccountswitcher/steam/data/remote/service/SteamAccountSwitcherAPIFactory.kt
@@ -1,8 +1,14 @@
 package com.sar.steamaccountswitcher.steam.data.remote.service
 
 import com.sar.steamaccountswitcher.steam.data.local.storage.WebAPIAddressStorage
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.KeyStore
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 // TODO: garbage solution, refactor this garbage
 class SteamAccountSwitcherAPIFactory(private val apiAddressStorage: WebAPIAddressStorage) {
@@ -25,9 +31,64 @@ class SteamAccountSwitcherAPIFactory(private val apiAddressStorage: WebAPIAddres
         val gsonConverterFactory = GsonConverterFactory.create()
         val retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(getUnsafeOkHttpClient())
             .addConverterFactory(gsonConverterFactory)
             .build()
 
         return retrofit.create(SteamAccountSwitcherAPI::class.java)
+    }
+
+
+    // TODO: unsafe replace this l8r | https://stackoverflow.com/a/63399149 | temporary solution for testing purposes
+    private fun getUnsafeOkHttpClient(): OkHttpClient {
+        return try {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts = arrayOf<TrustManager>(
+                object : X509TrustManager {
+                    @Throws(CertificateException::class)
+                    override fun checkClientTrusted(
+                        chain: Array<X509Certificate?>?,
+                        authType: String?
+                    ) {
+                    }
+
+                    @Throws(CertificateException::class)
+                    override fun checkServerTrusted(
+                        chain: Array<X509Certificate?>?,
+                        authType: String?
+                    ) {
+                    }
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate?>? {
+                        return arrayOf()
+                    }
+                }
+            )
+
+            // Install the all-trusting trust manager
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory = sslContext.socketFactory
+            val trustManagerFactory: TrustManagerFactory =
+                TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+            trustManagerFactory.init(null as KeyStore?)
+            val trustManagers: Array<TrustManager> =
+                trustManagerFactory.trustManagers
+            check(!(trustManagers.size != 1 || trustManagers[0] !is X509TrustManager)) {
+                "Unexpected default trust managers:" + trustManagers.contentToString()
+            }
+
+            val trustManager =
+                trustManagers[0] as X509TrustManager
+
+
+            val builder = OkHttpClient.Builder()
+            builder.sslSocketFactory(sslSocketFactory, trustManager)
+            builder.hostnameVerifier(HostnameVerifier { _, _ -> true })
+            builder.build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
     }
 }
