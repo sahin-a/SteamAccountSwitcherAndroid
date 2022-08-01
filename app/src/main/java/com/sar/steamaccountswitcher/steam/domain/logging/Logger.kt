627771a7793cb62ec6b0a8d0d741c9d77cd1a05f
sar <sar@starfinanz.de>
2022-08-01 20:31:34 +0200

    logger impl

---
+++ app/src/main/java/com/sar/steamaccountswitcher/steam/domain/logging/Logger.kt
@@ -0,0 +1,8 @@
+package com.sar.steamaccountswitcher.steam.domain.logging
+
+interface Logger {
+    fun i(tag: String, message: String)
+    fun d(tag: String, message: String)
+    fun w(tag: String, message: String? = null, throwable: Throwable? = null)
+    fun e(tag: String, message: String, throwable: Throwable? = null)
+}
